0b1a3a9c1748ed58b3eb6e8e06e2816d3f8c54a6
Andrew <odoleandrew@yahoo.com>
2023-06-23 23:22:07 +0100

    First commit

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const data = [
+    {
+      id: 1,
+      name: 'Sample Video File',
+      type: 'video',
+      path: "/file-server/sample-5s.mp4"
+    },
+    {
+      id: 2,
+      name: 'Sample Audio File',
+      type: 'audio',
+      path: "/file-server/file_example_MP3_700KB.mp3"
+    },
+    {
+      id: 3,
+      name: 'PDF Document File',
+      type: 'document',
+      path: "/file-server/Free_Test_Data_100KB_PDF.pdf"
+    },
+    {
+      id: 4,
+      name: 'Food Image File ',
+      type: 'image',
+      path: "/file-server/img2.JPG"
+    },
+    {
+        id: 5,
+        name: 'Flower Image File ',
+        type: 'image',
+        path: "/file-server/img6.jpg"
+    },
+    {
+        id: 6,
+        name: 'Flower Image File ',
+        type: 'image',
+        path: "/file-server/img7.jpeg"
+    },
+  ]
